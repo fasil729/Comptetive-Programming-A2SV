8e61b8906ee56ee6af1ea6db76e5730779b180f8
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-03-14 15:39:01 -0700

    Time: 30 ms (83.81%), Space: 13.8 MB (51.80%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,20 +1,18 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        def recursion(a, b):
-            if b == 1: 
-                return a
-            if b == 0:
-                return 1
-            
-            if b % 2 == 0:
-                ans = recursion(a, b // 2)
-                return ans * ans % mod
-            else:
-                ans = recursion(a, (b - 1) // 2)
-                return a * ans * ans % mod
-            
-        power, mod = n // 2, 10 ** 9 + 7
-        if n % 2 == 0:
-            return recursion(20, power) % mod
+        mod = 1000000007
+        cof = n / 2
+        if n == 1:
+            return 5
+        if n == 2:
+            return 20
+        if n % 2:
+                return 5 * self.countGoodNumbers(n - 1) % mod
+       
+        if cof % 2 == 0:
+            ans = self.countGoodNumbers(n // 2)
+            return ans * ans % mod
         else:
-            return recursion(5, power + 1) * recursion(4, power) % mod 
+            ans = self.countGoodNumbers((n-1) // 2)
+            return 20 * ans * ans % mod
+            
