dfc6fdc0f8655a001d537500dfa609c58ec79e2f
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-02 00:20:24 +0300

    Time: 355 ms (91.60%), Space: 64.2 MB (87.79%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxSumBST(self, root: TreeNode) -> int:
+        res = 0
+        def traverse(root):
+            '''return status_of_bst, size_of_bst, left_bound, right_bound'''
+            nonlocal res
+            if not root: return 1, 0, None, None # this subtree is empty
+            
+            ls, l, ll, lr = traverse(root.left)
+            rs, r, rl, rr = traverse(root.right)
+            
+            if ((ls == 2 and lr < root.val) or ls == 1) and ((rs == 2 and rl > root.val) or rs == 1):
+		        # this subtree is a BST
+                size = root.val + l + r
+                res = max(res, size)
+                return 2, size, (ll if ll is not None else root.val), (rr if rr is not None else root.val)
+            return 0, None, None, None # this subtree is not a BST
+        
+        traverse(root)
+        return res
+        
