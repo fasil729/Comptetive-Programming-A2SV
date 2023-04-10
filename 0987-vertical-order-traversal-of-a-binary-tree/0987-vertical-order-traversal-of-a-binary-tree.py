6e3adb05c119e446f9733e863ebf0d46dacad7fe
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-10 12:30:02 +0300

    Time: 45 ms (8.55%), Space: 14.2 MB (18.35%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -1,24 +1,16 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-    def __lt__(self, obj):
-        return self.val < obj.val
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#     def __lt__(self, obj):
+#         return self.val < obj.val
         
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         
         tree = defaultdict(list)
-        # def dfs(root, col):
-        #     if not root:
-        #         return
-        #     tree[col].append(root.val)
-        #     dfs(root.left, col - 1)
-        #     dfs(root.right, col + 1)
-        # dfs(root, 0)
-        # using bread first search using priority queue
         def bfs(root):
             heap = [(0, root.val, 0, 0, root)]
             heapq.heapify(heap)
