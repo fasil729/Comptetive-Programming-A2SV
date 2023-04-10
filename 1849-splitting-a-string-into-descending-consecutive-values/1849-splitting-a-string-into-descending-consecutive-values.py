287744910a10772645002effe4fedd743ab7a070
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-10 10:21:46 +0300

    Time: 39 ms (48.78%), Space: 14.1 MB (17.99%) - LeetHub

---
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -0,0 +1,29 @@
+class Solution:
+    def splitString(self, s: str) -> bool:
+        
+        n = len(s)
+        def helper(start, last):
+            print(start, last, n)
+            if start >= n:
+                return True
+            val = 0
+            for ind in range(start, n):
+                val = 10 * val + int(s[ind])
+                if val == 0 and ind != n - 1:
+                    continue
+                if val == last - 1:
+                    return helper(ind + 1, val)
+                if val > last:
+                    return False
+                
+            return False
+        val = 0
+        for ind in range(n - 1):
+            val = 10 * val + int(s[ind])
+            if helper(ind + 1, val):
+                return True
+        return False
+            
+    
+            
+            
