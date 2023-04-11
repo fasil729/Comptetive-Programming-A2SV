3ead0120c9258d933b3dfc98f9211949a88f6257
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-11 22:08:36 +0300

    Time: 35 ms (65.78%), Space: 14.2 MB (25.81%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -9,9 +9,10 @@ class Solution:
                 return
             res.append(arr.copy())
             for ind in range(first, n):
-                
+
                 arr.append(nums[ind])
                 backtrack(ind + 1, arr)
+                # print(arr, res, ind, first)
                 arr.pop()
                
         backtrack(0, arr)
