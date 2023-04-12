b899cc69a3bdee7120d080d021efce921e92f610
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-12 23:09:38 +0300

    Time: 225 ms (5.11%), Space: 15.9 MB (23.28%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,28 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        
+        def helper(start, end):
+            mid = start + (end - start) // 2
+            if start >= end:
+                return nums[start]
+            left = helper(start, mid)
+            right = helper(mid + 1, end )
+            count = 0
+            if left == right:
+                return left
+            else:
+                right_count = 0
+                left_count = 0
+                for num in nums[start:end + 1]:
+                    if num == left:
+                        left_count += 1
+                    elif num == right:
+                        right_count += 1
+                if left_count > right_count:
+                    return left
+                return right
+        return helper(0, len(nums) - 1)
+                
+                        
+        
