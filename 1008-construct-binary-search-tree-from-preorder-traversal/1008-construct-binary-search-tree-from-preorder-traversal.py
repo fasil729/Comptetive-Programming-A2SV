391a7368fa9e8cac52ac83d3a02b94ee3f24f78a
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-19 23:31:10 +0300

    Time: 38 ms (63.37%), Space: 13.9 MB (79.19%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -11,31 +11,21 @@ class Solution:
         node = root
         stack = [node]
         for ind, val in enumerate(preorder[1:]):
-            
             next_node = TreeNode(val)
             if node.val > next_node.val:
                 childs.add(next_node)
                 node.left = next_node
             stack.append(next_node)
-            # print([i.val for i in stack], next_node.val, val)
-            
-
             new_node = next_node
             for i in range(len(stack)):
                 
-                # print(new_node.val)
                 new_node = stack[i]
                 if val > new_node.val:
-                    # print("here")
                     if next_node not in childs:
                         new_node.right = next_node
                     stack.insert(i, next_node)
                     stack.pop()
                     break
-                # else:
-                #     stack[i], stack[i + 1] = stack[i + 1], stack[i]
-                
-            # print([i.val for i in stack], next_node.val)
             node = next_node
         return root
                     
