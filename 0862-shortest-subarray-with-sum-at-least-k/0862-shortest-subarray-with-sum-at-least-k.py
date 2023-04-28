f84581ce20988ec17fbdb448acc4cb81f24f7400
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-04-28 22:06:05 +0300

    Time: 2444 ms (5.10%), Space: 36.1 MB (5.95%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,8 +1,6 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        print("new")
         res = len(nums) + 1
-        ed, beg = 0, 0
         sum = 0
         heap = [(sum, -1)]
         heapq.heapify(heap)
