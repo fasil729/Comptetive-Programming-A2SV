d8e86d66cd1e55df7c7407b91e15dbbbb1cd25a3
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-05-02 22:16:21 +0300

    Time: 51 ms (5.32%), Space: 16.4 MB (6.17%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,8 @@
 class Solution:
+    @cache
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        if n <= 1:
+            return x ** n
+        mid = n // 2
+        return self.myPow(x, mid) * self.myPow(x, n - mid)
         
