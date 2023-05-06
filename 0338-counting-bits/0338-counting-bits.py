a817a3d2eaff39c917448f5d2736797c878d9431
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-05-06 21:00:38 +0300

    Time: 80 ms (83.09%), Space: 23.1 MB (9.63%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0]
+        j = 0
+        arr = set([0])
+        num = 2
+        while num <= n:
+            arr.add(num)
+            num *= 2
+        print(arr)
+        for i in range(1, n + 1):
+            if i in arr:
+                j = 0
+            ans.append(ans[j] + 1)
+            j += 1
+            
+        return ans
+            
