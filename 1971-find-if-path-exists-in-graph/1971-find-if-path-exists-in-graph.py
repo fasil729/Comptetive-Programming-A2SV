72a2d98b6cd75b7749f3de07dbd76798e25c7c93
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-07-08 04:58:27 +0300

    Time: 1942 ms (65.44%), Space: 107.3 MB (80.87%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,19 +1,39 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [0] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+       
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+                
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+
+
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adjacency_list = defaultdict(list)
-        for n_1, n_2 in edges:
-            adjacency_list[n_1].append(n_2)
-            adjacency_list[n_2].append(n_1)
+        # Approach the question using UNIONFIND method
+        uf = UnionFind(n)
         
-        visited = set()
-        def dfs(node):
-            if node == destination:
-                return True
-            visited.add(node)
-            for neigh in adjacency_list[node]:
-                if not neigh in visited and dfs(neigh):
-                    return True
-            return False
-        return dfs(source)
+        for v, u in edges:
+            uf.union(u, v)
+        return uf.find(source) == uf.find(destination)
+            
                 
             
