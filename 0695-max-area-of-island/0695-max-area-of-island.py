6fe4a5f097505015acfddd2ad32188a83d384b45
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-07-08 23:18:46 +0300

    Time: 179 ms (17.57%), Space: 17.1 MB (76.73%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,4 +1,56 @@
+class UnionFind:
+    def __init__(self, n, m, grid):
+        self.root = {(i, j):(i, j) for i in range(n) for j in range(m)}
+        self.rank =  {(i, j):1 if grid[i][j] else 0 for i in range(n) for j in range(m)}
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x]) # path compression
+        return self.root[x]
+       
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:   # optimizing union functtion using rank
+                self.root[rootY] = rootX
+                self.rank[rootX] += self.rank[rootY]
+        
+            else:
+                self.root[rootX] = rootY
+                self.rank[rootY] += self.rank[rootX]
+
 class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        uf = UnionFind(n, m, grid)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    if i < n - 1 and grid[i + 1][j]:
+                        uf.union((i, j), (i + 1, j))
+                    if j < m - 1 and grid[i ][j + 1]:
+                        uf.union((i, j), (i , j + 1))
+        ans = 0
+        for val in uf.rank.values():
+            ans = max(ans, val)
+        return ans
+            
+        
+                
+
+
+
+
+
+
+
+
+
+
+class Solution2:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
         m = len(grid)
