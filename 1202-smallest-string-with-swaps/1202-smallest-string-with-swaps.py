dd928451ae87f93fb1d5781489e3f872e3e13ad3
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-07-15 14:12:10 +0300

    Time: 767 ms (30.31%), Space: 52.8 MB (66.67%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,23 +1,39 @@
-class Solution:
-    def union(self, a, b):
-        self.parent[self.find(a)] = self.find(b)
-		
-    def find(self, a):
-        if self.parent[a] != a:
-            self.parent[a] = self.find(self.parent[a])
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [0 for i in range(size)]
 
-        return self.parent[a]
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x]) # path compression
+        return self.root[x]
+       
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:   # optimizing union functtion using rank
+                self.root[rootY] = rootX
+                
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+class Solution:
         
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
 		# 1. Union-Find
-        self.parent = list(range(len(s)))
+        uf = UnionFind(len(s))
         for a, b in pairs:
-            self.union(a, b)
+            uf.union(a, b)
 
 		# 2. Grouping
         group = defaultdict(lambda: ([], []))  
         for i, ch in enumerate(s):
-            parent = self.find(i)
+            parent = uf.find(i)
             group[parent][0].append(i)
             group[parent][1].append(ch)
 
