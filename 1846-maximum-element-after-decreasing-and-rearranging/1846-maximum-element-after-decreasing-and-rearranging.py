3f94e994bd49bea63119f521cbb415d754123bb1
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-07-15 22:57:50 +0300

    Time: 619 ms (5.85%), Space: 45.9 MB (5.26%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -1,5 +1,27 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        count = defaultdict(int)
+        greaterl = 0
+        for num in arr:
+            count[num]+=1
+            if num>len(arr):
+                greaterl+=1
+        ans = len(arr)
+        i = len(arr)
+        while i>1:
+            if count[i]:
+                greaterl+=(count[i]-1)
+            else:
+                if greaterl>0:
+                    greaterl-=1
+                else:
+                    ans-=1
+            i-=1
+        return ans
+
+
+class Solution2:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         arr.sort()
         maxi = 0
         ind = 0
