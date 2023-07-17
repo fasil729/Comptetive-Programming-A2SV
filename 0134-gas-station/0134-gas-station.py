da0512d97c1dee247ae4c9619ed8f2908ed285f5
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-07-17 13:29:52 +0300

    Time: 1266 ms (25.57%), Space: 22.2 MB (74.39%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,5 +1,25 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        # greedy approach and find the smallest index with diff > 0
+        ans, total = 0, 0
+
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+
+            if total < 0:
+                # fail -> reset
+                total = 0
+                ans = i + 1
+        
+        return ans
+
+
+
+class Solution2:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if len(gas) == 1:
             return 0 if gas[0] - cost[0] >= 0 else -1
         res = -1
