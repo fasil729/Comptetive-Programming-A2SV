92de594df956af7b82cda0e66c578e5c6d506d32
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-02 17:11:14 +0300

    Time: 65 ms (86.50%), Space: 18.3 MB (12.68%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,15 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
         
-        @cache
+        @lru_cache
         def dp(index):
-            if index >= n:
+            if index >= len(cost):
                 return 0
             
-            
             return cost[index] + min(dp(index + 1), dp(index + 2))
-        return min(dp(0), dp(1))
-            
-            
         
+        
+        return min(dp(0) , dp(1))
