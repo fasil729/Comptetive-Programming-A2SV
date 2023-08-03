eeef054d94deed4647c08649ab33f2ad1a07fc11
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-03 23:03:18 +0300

    Time: 493 ms (99.01%), Space: 30 MB (88.14%) - LeetHub

---
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        
+        
+        # bottom up dp approach
+        dp = {}
+        maxi = 1
+        for i in arr:
+            if i - difference not in dp:
+                dp[i] = 1
+            else:
+                dp[i] = 1 + dp[i - difference]
+            maxi = max(maxi, dp[i])
+        
+        return maxi
+        
+        
