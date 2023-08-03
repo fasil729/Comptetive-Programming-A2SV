0dae90f8d5a489b30e94ed5fdb2a9dbd05ec2e2a
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-03 23:40:23 +0300

    Time: 69 ms (91.52%), Space: 17.4 MB (72.16%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
+    def minimumTotal2(self, triangle: List[List[int]]) -> int:
         
         # top down dp approach
         @lru_cache
@@ -10,3 +10,17 @@ class Solution:
             return triangle[row][col] + min(dp(row + 1, col), dp(row + 1, col + 1))
         
         return dp(0, 0)
+    
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+        # bottom up dp approach
+        prev_dp = triangle[-1]
+        m = len(triangle) - 2
+        for row in range(m, -1, -1):
+            dp = []
+            for col in range(row + 1):
+                dp.append(min(prev_dp[col], prev_dp[col + 1]) + triangle[row][col])
+            prev_dp = dp
+        
+        return prev_dp[0]
+            
