ec2018ee90816efc8585d03fc836153913425c72
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-08 01:01:45 +0300

    Time: 1183 ms (74.50%), Space: 17.5 MB (30.50%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,5 +1,5 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
+    def coinChange1(self, coins: List[int], amount: int) -> int:
         
         # top down approach
         @cache
@@ -14,3 +14,14 @@ class Solution:
             return ans
         res =  dp(amount)
         return res if res != inf else -1
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        # bottom up approach
+        dp = defaultdict(lambda: inf)
+        dp[0] = 0
+        for n in range(amount + 1):
+            for c in coins:
+                if c <= n:
+                    dp[n] = min(dp[n], dp[n - c] + 1)
+        return dp[amount] if dp[amount] != inf else -1
+    
