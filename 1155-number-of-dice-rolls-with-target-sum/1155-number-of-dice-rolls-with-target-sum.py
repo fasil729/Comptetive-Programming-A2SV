d71ba9e207e56668cdcfd7a646d59876e9c1b1b5
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-09 12:18:41 +0300

    Time: 2060 ms (5.04%), Space: 20.6 MB (41.12%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+    def numRollsToTarget1(self, n: int, k: int, target: int) -> int:
         # top down dp
         @cache
         def dp(n, target):
@@ -13,5 +13,29 @@ class Solution:
                     res += dp(n - 1, target - j)
             return res
         return dp(n, target) % (10 ** 9 + 7)
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        
+        def get(t, n):
+            if (t, n) in dp:
+                return dp[(t, n)]
+            return 0
+        
+        # bottom up approach
+        dp = {(0, 0):1}
+        c = 0
+            
+        for t in range(1, target + 1):
+            for d in range(1, k + 1):
+                for c in range(0, n + 1):
+                    if not (t, c) in dp:
+                        dp[(t, c)] = 0
+                    if d <= t and n >= c: 
+
+                        dp[(t, c)] += get(t - d, c - 1)
+        return dp[(target, n)] % (10 ** 9 + 7)
+            
+    
+    
+    
                 
         
