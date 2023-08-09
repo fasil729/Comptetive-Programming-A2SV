963f0f075e67a1eb0ea164f691c5e906431d766c
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-09 06:11:27 +0300

    Time: 40 ms (75.91%), Space: 16.4 MB (31.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,8 @@
 class Solution:
     def __init__ (self):
             self.memo = {}
-    def climbStairs(self, n: int) -> int:
+    def climbStairs1(self, n: int) -> int:
+        # top down dp
         if n in self.memo:
             return self.memo[n]
         if n == 0:
@@ -10,3 +11,19 @@ class Solution:
             return 0
         self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
         return self.memo[n]
+    
+    def climbStairs(self, n: int) -> int:
+        #bottom up dp
+        first, second = 0, 1
+        for i in range(1, n + 1):
+            third = second + first
+            first = second
+            second = third
+        return third
+            
+            
+            
+        
+    
+    
+        
