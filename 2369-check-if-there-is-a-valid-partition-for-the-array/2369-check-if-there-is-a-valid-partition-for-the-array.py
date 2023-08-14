a059e760e0016f1b36429daf2ed9c7880c50c71c
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-14 09:11:18 +0300

    Time: 997 ms (72.87%), Space: 38.8 MB (48.06%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,5 +1,5 @@
 class Solution:
-    def validPartition(self, nums: List[int]) -> bool:
+    def validPartition2(self, nums: List[int]) -> bool:
         n = len(nums)
         # top down dp approach
         @cache
@@ -16,6 +16,25 @@ class Solution:
                 return dp(index + 2)
             return False
         return dp(0)
+    
+    def validPartition(self, nums: List[int]) -> bool:
+        # bottom up dp approach
+        n = len(nums)
+        dp = {n: True}
+        for index in range(n - 1, -1, -1):
+            if index not in dp:
+                dp[index] = False
+            if index < n - 2:
+                if nums[index] == nums[index + 1] == nums[index + 2]:
+                    dp[index] = dp[index] or dp[index + 3]
+                elif nums[index] + 2 == nums[index + 1] + 1 == nums[index + 2]:
+                    dp[index] = dp[index] or dp[index + 3]
+                    
+            if index < n - 1 and nums[index] == nums[index + 1]:
+                dp[index] = dp[index] or dp[index + 2]
+        
+        return dp[0]
+            
             
     
        
