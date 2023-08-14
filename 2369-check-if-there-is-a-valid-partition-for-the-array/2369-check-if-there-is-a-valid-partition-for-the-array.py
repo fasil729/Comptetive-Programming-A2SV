bfbe34d28537a1d9fc0816ebc690cba1e32814a8
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-14 08:55:34 +0300

    Time: 882 ms (92.25%), Space: 124.6 MB (13.95%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,19 +1,22 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-        dp = [True] + [False] * n
-
-        # Determine if the prefix array nums[0 ~ i] has a valid partition
-        for i in range(n):
-            dp_index = i + 1
-
-            # Check 3 possibilities
-            if i > 0 and nums[i] == nums[i - 1]:
-                dp[dp_index] |= dp[dp_index - 2]
-            if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
-                dp[dp_index] |= dp[dp_index - 3]
-            if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
-                dp[dp_index] |= dp[dp_index - 3]
- 
-        return dp[n]
+        # top down dp approach
+        @cache
+        def dp(index):
+            if index == n:
+                return True
+            if index < n - 2:
+                if nums[index] == nums[index + 1] == nums[index + 2]:
+                    return dp(index + 2) or dp(index + 3)
+                elif nums[index] + 2 == nums[index + 1] + 1 == nums[index + 2]:
+                    return dp(index + 3)
+                    
+            if index < n - 1 and nums[index] == nums[index + 1]:
+                return dp(index + 2)
+            return False
+        return dp(0)
+            
+    
+       
         
