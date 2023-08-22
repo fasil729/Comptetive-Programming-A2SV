99af1067029be1571d7b662714af450bf7d75d8f
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-22 17:43:21 +0300

    Time: 179 ms (80.77%), Space: 16.8 MB (76.26%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -1,5 +1,7 @@
 class Solution:
-    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+    def updateBoard1(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        # using dfs approach
+        
         row, col = click
         visited = set()
         n = len(board)
@@ -39,5 +41,49 @@ class Solution:
 
         dfs(row, col)
         return board
+    
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        # using bfs approach
+        
+        row, col = click
+        visited = set()
+        n = len(board)
+        m = len(board[0])
+
+        if board[row][col] == "M":
+            board[row][col] = "X"
+            return board
+        queue = deque([(row, col)])
+        
+        while queue:
+            direc = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]
+            adj_mines = 0
+            row, col = queue.popleft()
+            if board[row][col] == "M":
+                continue
+
+            # count adjacent mines
+            for r, c in direc:
+                n_r = row + r
+                n_c = col + c
+                if  0 <= n_r < n and 0 <= n_c < m and board[n_r][n_c] == "M":
+                    adj_mines += 1
+
+            # check if there are adjacent mines
+            if adj_mines:
+                board[row][col] = str(adj_mines)
+                continue
+                
+            board[row][col] = "B"    
+            for r, c in direc:
+                n_r = row + r
+                n_c = col + c
+                if  0 <= n_r < n and 0 <= n_c < m and not (n_r, n_c) in visited:
+                    visited.add((n_r, n_c))
+                    queue.append((n_r, n_c))
+        return board
+                    
+            
+        
                     
                 
