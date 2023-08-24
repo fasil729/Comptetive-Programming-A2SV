9702ddf6ec105a7e73ae359283c24a520d507350
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-08-25 00:14:01 +0300

    Time: 45 ms (71.87%), Space: 16.4 MB (41.73%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,9 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1) * int(num2))
+        res = 0
+        power = 0
+        for num in num1[::-1]:
+            res += int(num) * int(num2) * 10 ** power
+            power += 1
+        return str(res)
         
