c87efd8276fc2cd67f71c537513279c9fbd74698
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-09-01 10:13:08 +0300

    Time: 84 ms (61.34%), Space: 22.8 MB (99.76%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,17 +1,7 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0]
-        j = 0
-        arr = set([0])
-        num = 2
-        while num <= n:
-            arr.add(num)
-            num *= 2
-        for i in range(1, n + 1):
-            if i in arr:
-                j = 0
-            ans.append(ans[j] + 1)
-            j += 1
-            
-        return ans
-            
+        # bruteforce approach
+        res = []
+        for i in range(n + 1):
+            res.append(bin(i).count("1"))
+        return res        
