28a2e51d297c04593864566feedfc656895a6338
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-09-02 11:34:06 +0300

    Time: 567 ms (16.66%), Space: 16.8 MB (41.01%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -4,20 +4,19 @@ class Solution:
         n = len(s)
         dictionary = set(dictionary)
         @cache
-        def dp(index, extra):
+        def dp(index):
             if index >= n:
-                return extra
+                return 0
             temp = ""
             res = n - index
             for i in range(index, n):
                 temp += s[i]
                 for j in range(len(temp)):
                     if temp[j:] in dictionary:
-                        res = min(res, dp(i + 1, j))
-                        break
+                        res = min(res, dp(i + 1) + j)
                         
-            return extra + res
-        return dp(0, 0)
+            return res
+        return dp(0)
                     
                 
         
