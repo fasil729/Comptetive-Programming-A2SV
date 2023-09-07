003df21a4c00ee620a115cf536a4c3505b0100b6
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-09-07 17:25:21 +0300

    Time: 46 ms (28.80%), Space: 16.4 MB (91.05%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,35 +5,21 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head, left, right):
-        def reverse(head, steps):
-            count = 0
-            curr = head
-            prev = None
-            while count < steps:
-                curr_next = curr.next
-                curr.next = prev
-                prev = curr
-                curr = curr_next
-                count += 1
-
-        prev_left, left_node, right_node = None, None, None
-        curr = head
-        for index in range(1, right + 1):
-            if index == left - 1:
-                prev_left = curr
-            if index == left:
-                left_node = curr
-            if index == right:
-                right_node = curr
-            curr = curr.next
-
-        next_right = right_node.next
-        reverse(left_node, right - left + 1)
-        left_node.next = next_right
-
-        if prev_left:
-            prev_left.next = right_node
-            return head
-        else:
-            return right_node
+        
+        dummy = ListNode(0, head)
+        
+        leftPrev, cur = dummy, head
+        for i in range(left - 1):
+            leftPrev, cur = cur, cur.next
+            
+        prev = None
+        for i in range(right - left + 1):
+            nex = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nex
+        leftPrev.next.next, leftPrev.next = cur, prev
+        return dummy.next
+        
+            
         
