509538ab37aa9d8e85c0881b7f2391afdd865f7a
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-09-18 14:03:51 +0300

    Time: 65 ms (70.18%), Space: 17.5 MB (49.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # O(n**2)
-        n = len(nums)
-        for ind in range(n):
-            for j in range(ind + 1, n):
-                if nums[j] + nums[ind] == target:
-                    return [ind, j]
+        # # O(n**2)
+        # n = len(nums)
+        # for ind in range(n):
+        #     for j in range(ind + 1, n):
+        #         if nums[j] + nums[ind] == target:
+        #             return [ind, j]
+        
+        # O(n)
+        memo = {}
+        for ind, num in enumerate(nums):
+            diff = target - num
+            if diff in memo:
+                return [memo[diff], ind]
+            memo[num] = ind
+        
+                
+                
     
     
             
