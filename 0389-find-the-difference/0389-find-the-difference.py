06d4112bed3e53be92fab12c466c6bff34c40abd
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-09-25 19:30:29 +0300

    Time: 37 ms (82.89%), Space: 16.2 MB (76.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,17 @@
 class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
+    def findTheDifference1(self, s: str, t: str) -> str:
+        # hash table
         count_t = Counter(t)
         count_s = Counter(s)
         for char in count_t:
             if char not in count_s or count_s[char] != count_t[char]:
                 return char
+    def findTheDifference(self, s: str, t: str) -> str:
+        # bit manuplation
+        bit_map = 0
+        for char in s + t:
+            bit_map ^= ord(char)
+        return chr(bit_map)
+            
+
         
