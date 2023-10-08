319119b774415b142f137ff25090a1a02bf3e012
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-08 21:24:28 +0300

    Time: 376 ms (50.00%), Space: 52 MB (36.62%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+    def maxDotProduct1(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         m = len(nums2)
         # top down approach
@@ -21,4 +21,22 @@ class Solution:
         if min(nums1) > 0 and max(nums2) < 0:
             return min(nums1) * max(nums2)
         return dp(0, 0)
+    
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        # bottom up approach
+        m, n = len(nums2), len(nums1)
+        dp = {}
+        for i in range(n, -1, -1):
+            for j in range(m, -1, -1):
+                if i == n or j == m:
+                    dp[(i, j)] = 0
+                else:
+                    use = nums1[i] * nums2[j] + dp[(i + 1, j + 1)]
+                    dp[(i, j)] = max(use, dp[(i, j + 1)], dp[(i + 1, j)])
+        if max(nums1) < 0 and min(nums2) > 0:
+            return max(nums1) * min(nums2)
+        
+        if min(nums1) > 0 and max(nums2) < 0:
+            return min(nums1) * max(nums2)
+        return dp[(0, 0)]
             
