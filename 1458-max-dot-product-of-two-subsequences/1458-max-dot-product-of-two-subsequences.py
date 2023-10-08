e1dab97a4ae0f515c0220997bd9a60c679957628
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-08 21:41:39 +0300

    Time: 169 ms (99.30%), Space: 16.3 MB (98.59%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -7,14 +7,13 @@ class Solution:
         def dp(ind1, ind2):
             if ind1 >= n or ind2 >= m:
                 return 0
+            
             prod = nums1[ind1] * nums2[ind2]
+            
             ans = dp(ind1 + 1, ind2 + 1) + prod
+            
             return max(ans, dp(ind1 + 1, ind2), dp(ind1, ind2 + 1))
-            
-                
-            
-            
-            return ans
+        
         if max(nums1) < 0 and min(nums2) > 0:
             return max(nums1) * min(nums2)
         
@@ -25,18 +24,20 @@ class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
         # bottom up approach
         m, n = len(nums2), len(nums1)
-        dp = {}
-        for i in range(n, -1, -1):
-            for j in range(m, -1, -1):
-                if i == n or j == m:
-                    dp[(i, j)] = 0
-                else:
-                    use = nums1[i] * nums2[j] + dp[(i + 1, j + 1)]
-                    dp[(i, j)] = max(use, dp[(i, j + 1)], dp[(i + 1, j)])
+        prev_dp = [0] * (m + 1)
+        dp = [0] * (m + 1)
+        for i in range(n - 1, -1, -1):
+            dp = [0] * (m + 1)
+            for j in range(m - 1, -1, -1):
+                use = nums1[i] * nums2[j] + prev_dp[j + 1]
+                dp[j] = max(use, dp[j + 1], prev_dp[j])
+            prev_dp = dp
+                
+            
         if max(nums1) < 0 and min(nums2) > 0:
             return max(nums1) * min(nums2)
         
         if min(nums1) > 0 and max(nums2) < 0:
             return min(nums1) * max(nums2)
-        return dp[(0, 0)]
+        return dp[0]
             
