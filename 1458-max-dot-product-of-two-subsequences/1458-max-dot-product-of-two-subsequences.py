8894d4aa74e359f8066d5d013755155ee4129622
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-08 20:49:33 +0300

    Time: 566 ms (16.20%), Space: 99.3 MB (19.01%) - LeetHub

---
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+        m = len(nums2)
+        
+        @cache
+        def dp(ind1, ind2):
+            if ind1 >= n or ind2 >= m:
+                return 0
+            ans =  max(dp(ind1 + 1, ind2 + 1), dp(ind1 + 1, ind2), dp(ind1, ind2 + 1))
+            prod = nums1[ind1] * nums2[ind2]
+            ans = max(ans, dp(ind1 + 1, ind2 + 1) + prod)
+                
+            
+            
+            return ans
+        if max(nums1) < 0 and min(nums2) > 0:
+            return max(nums1) * min(nums2)
+        
+        if min(nums1) > 0 and max(nums2) < 0:
+            return min(nums1) * max(nums2)
+        return dp(0, 0)
+            
