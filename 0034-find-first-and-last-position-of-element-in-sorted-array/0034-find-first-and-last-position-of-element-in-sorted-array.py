c82cf1e86ff16cedccfd6eaed35628f0673836dc
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-09 15:02:48 +0300

    Time: 83 ms (63.27%), Space: 17.9 MB (14.98%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,23 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binary(nums, target, rightbias):
-            low, high = 0, len(nums) - 1
-            ind = -1
-            while low <= high:
-                mid = low + (high - low) // 2
-                
-                if nums[mid] < target:
-                    low = mid + 1
-                elif nums[mid] > target:
-                    high = mid - 1
-                else:
-                    ind = mid
-                    if rightbias:
-                        low = mid + 1
-                    else:
-                        high = mid - 1
-            return ind
-        left = binary(nums, target, False)
-        right = binary(nums, target, True)
-        return [left, right]
-            
+        if not nums:
+            return [-1, -1]
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            # print(l, r)
+            mid = (l + r) // 2
+            print(l, r, mid)
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r= mid - 1
+        left = l
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            print(l, r)
+            mid = (l + r) // 2
+            if nums[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        if left == l:
+            return [-1, -1]
+        return [left, l - 1]
+        
