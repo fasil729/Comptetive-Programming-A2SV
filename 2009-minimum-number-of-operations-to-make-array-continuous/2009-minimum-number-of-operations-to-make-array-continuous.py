ca9527c88e137c07d110fe7d52755555fa01b958
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-10 17:20:18 +0300

    Time: 700 ms (65.79%), Space: 35.5 MB (38.82%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -32,7 +32,7 @@ class Solution:
                 ans = min(ans, n - (right - i))
             return ans
         
-    def minOperations(self, nums: List[int]) -> int:
+    def minOperations2(self, nums: List[int]) -> int:
             n = len(nums)
             ans = n
             new_nums = sorted(set(nums))
@@ -45,5 +45,19 @@ class Solution:
                 ans = min(ans, n - count)
 
             return ans
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = n
+        new_nums = sorted(set(nums))
+        j = 0
+        
+        for i in range(len(new_nums)):
+            while j < len(new_nums) and new_nums[j] < new_nums[i] + n:
+                j += 1
+            
+            count = j - i
+            ans = min(ans, n - count)
+
+        return ans
                 
                 
