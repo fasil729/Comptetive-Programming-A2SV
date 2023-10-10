3c201c36c39371db7422010ba11384622e24dc0c
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-10 17:09:23 +0300

    Time: 732 ms (46.05%), Space: 35.4 MB (38.82%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
+    def minOperations1(self, nums: List[int]) -> int:
             # sort the array
             nums.sort()
             duplicate = set()
@@ -30,6 +30,20 @@ class Solution:
             for i in range(m + 1):
                 right = binary_search(i, m, new_num[i] + n - 1)
                 ans = min(ans, n - (right - i))
+            return ans
+        
+    def minOperations(self, nums: List[int]) -> int:
+            n = len(nums)
+            ans = n
+            new_nums = sorted(set(nums))
+
+            for i in range(len(new_nums)):
+                left = new_nums[i]
+                right = left + n - 1
+                j = bisect_right(new_nums, right)
+                count = j - i
+                ans = min(ans, n - count)
+
             return ans
                 
                 
