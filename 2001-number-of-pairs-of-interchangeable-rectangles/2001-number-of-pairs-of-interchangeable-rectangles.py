27555ea34aa47367bfe231e720c5ebda7bc65716
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-25 12:17:30 +0300

    Time: 1411 ms (92.57%), Space: 73.1 MB (24.63%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -1,11 +1,11 @@
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
-        ratio = defaultdict(int)  # ratio 
+        ratio = defaultdict(int)  # ratio
+        ans = 0
         for w, h in rectangles: # ratio  1/2:4
             r = w / h
+            ans += ratio[r]
             ratio[r] += 1
-        ans = 0
-        for val in ratio.values():
-            ans += (val * (val - 1)) // 2
+        
         return ans
         
