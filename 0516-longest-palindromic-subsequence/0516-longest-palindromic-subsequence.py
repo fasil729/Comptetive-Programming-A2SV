553f7d8df087392913c88855a089e6a52401ba06
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-27 14:35:35 +0300

    Time: 868 ms (82.43%), Space: 16.2 MB (97.65%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
+    def longestPalindromeSubseq1(self, s: str) -> int:
+        # top down dp approach
         n = len(s)
         @cache
         def dp(start, end):
@@ -13,5 +14,33 @@ class Solution:
             return max(dp(start + 1, end), dp(start, end - 1))
         
         return dp(0, n - 1)
+    
+    def longestPalindromeSubseq2(self, s: str) -> int:
+        n = len(s)
         
+        dp = [0] * n
+        for start in range(n - 1, -1, -1):
+            prev = dp
+            for end in range(start + 1, n):
+                if s[start]  == s[end]:
+                    dp[end] = prev[end - 1] + 2
+                else:
+                    dp[end] = max(prev[end], dp[end - 1])
+        print(dp)
+        return prev[-2]
+    def longestPalindromeSubseq(self, s: str) -> int:
+            n = len(s)
+            dp = [1] * n
+            for j in range(1, n):
+                    pre = dp[j]
+                    for i in range(j-1, -1, -1):
+                        tmp = dp[i]
+                        if s[i] == s[j]:
+                            dp[i] = 2 + pre if i + 1 <= j - 1 else 2
+                        else:
+                            dp[i] = max(dp[i + 1], dp[i])
+                        pre = tmp
+            return dp[0]
+
+    
         
