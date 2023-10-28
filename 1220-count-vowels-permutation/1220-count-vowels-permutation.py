5866ee4ae6eedfebfbbbfb3a77bbd32823a34504
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-28 12:28:44 +0300

    Time: 298 ms (51.94%), Space: 16.3 MB (86.67%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,5 +1,6 @@
 class Solution:
-    def countVowelPermutation(self, n: int) -> int:
+    def countVowelPermutation1(self, n: int) -> int:
+        # top down dp approach
         graph = {}
         mod = 10 ** 9 + 7
         
@@ -24,6 +25,31 @@ class Solution:
             ans += dp(node, n)
             
         return ans % mod
-            
+    
+    def countVowelPermutation(self, n: int) -> int:
+        # top down dp approach
+        graph = {}
+        mod = 10 ** 9 + 7
+        
+        graph["a"] = ["e"]
+        graph["e"] = ["a", "i"]
+        graph["i"] = ["a", "e", "o", "u"]
+        graph["o"] = ["i", "u"]
+        graph["u"] = ["a"]
+        vowels = ["a", "e", "i", "o", "u"]
+        
+        dp = {v:1 for v in vowels}
+        
+        
+        for i in range(2, n + 1):
+            prev = dp.copy()
+            for node in vowels:
+                dp[node] = 0
+                for neigh in graph[node]:
+                    dp[node] += prev[neigh] 
+                
+                dp[node] %= mod
+        return sum(dp.values()) % mod
+                
         
         
