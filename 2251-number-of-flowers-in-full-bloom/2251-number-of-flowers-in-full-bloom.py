58f512ada9ccd5942da168f5e40cbb8ebe7127b7
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-10-31 16:24:25 +0300

    Time: 1263 ms (8.70%), Space: 44.5 MB (66.64%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,24 +1,43 @@
-from sortedcontainers import SortedDict
-
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        difference = SortedDict({0: 0})
+        arr = []
         for start, end in flowers:
-            difference[start] = difference.get(start, 0) + 1
-            difference[end + 1] = difference.get(end + 1, 0) - 1
+            arr.append(start)
+            arr.append(end + 1)
+        arr.append(0)
+        arr = list(set(arr))
+        arr.sort()
+        n = len(arr)
         
-        positions = []
-        prefix = []
-        curr = 0
-        for key, val in difference.items():
-            positions.append(key)
-            curr += val
-            prefix.append(curr)
-
+        def get_index(num):
+            l, r = 0, len(arr) - 1
+            
+            while l <= r:
+                mid = l + (r - l) // 2
+                if arr[mid] <= num:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            return l
+        
+        pref = [0] * (n + 1)
+        for start, end in flowers:
+            i = get_index(start) - 1
+            j = get_index(end) - 1
+            pref[i] += 1
+            if j < n - 1:
+                pref[j + 1] -= 1
+        print(pref)
+        for i in range(1, n + 1):
+            pref[i] += pref[i - 1]
+        
+        print(pref)
+        print(arr)
+        
         ans = []
-        for person in people:
-            i = bisect_right(positions, person) - 1
-            ans.append(prefix[i])
-        
+        for p in people:
+            ind = get_index(p) - 1
+            ans.append(pref[ind])
         return ans
         
