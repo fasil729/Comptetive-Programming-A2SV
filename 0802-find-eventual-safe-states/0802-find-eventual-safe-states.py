ce6ed8f768e41328037262f47cfc0182e6eedd22
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-02 22:39:58 +0300

    Time: 590 ms (48.97%), Space: 23.8 MB (56.64%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,31 +1,23 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n = len(graph)
-        colors = [0 for _ in range(n)]
-        order = []
-
         
-
-        for node in range(n):
-            if colors[node] != 0:
-                continue
-            self.topSort(node, colors, graph, order)
-                
-
-        return sorted(order)
-    def topSort(self, node, colors, graph, order):
-        # Cycle found - Why?
-        if colors[node] == 1:
-            return False
-
-        colors[node] = 1
-        for neighbor in graph[node]:
-            if colors[neighbor] == 2:
-                continue
-            if not self.topSort(neighbor, colors, graph, order):
+        
+        n = len(graph)
+        @cache
+        def dfs(node):
+            if not graph[node]:
+                return True
+            if node in visited:
                 return False
-
-        # Color nodes black as we backtrack
-        colors[node] = 2
-        order.append(node)
-        return True
+            visited.add(node)
+            ans = True
+            for neigh in graph[node]:
+                ans = ans and dfs(neigh)
+            return ans
+        ans = []
+        for i in range(n):
+            visited = set()
+            if dfs(i):
+                ans.append(i)
+        return ans
+        
