2bad9c3cfada4bec820cb21ca34e594e20ad8602
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-10 22:44:13 +0300

    Time: 1939 ms (28.22%), Space: 16.5 MB (80.10%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,15 +1,44 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition1(self, nums: List[int]) -> bool:
         
+        half = sum(nums) / 2
+        n = len(nums)
+        # top down
         @cache
-        def dp(index, half_sum):
-            if index == len(nums) or half_sum == 0:
-                return half_sum == 0
-            if half_sum < 0:
+        def dp(ind, tot):
+            if tot == 0:
+                return True
+            if ind == n or tot < 0:
                 return False
             
+            return dp(ind + 1, tot) or dp(ind + 1, tot - nums[ind])
+        
+        return dp(0, half)
             
-            return dp(index + 1, half_sum - nums[index]) or dp(index + 1, half_sum)
-        return dp(0,  sum(nums) / 2)
+    
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+        half = sum(nums) // 2
         
+        n = len(nums)
+        # bottom up
+        dp = [False for i in range(half + 1)]
+        dp[0] = True
         
+        for ind in range(n - 1, -1, -1):
+            prev = dp.copy()
+            
+            
+            for half_sum in range(half + 1):
+                dp[half_sum] = False
+                dp[half_sum] = prev[half_sum]
+                if half_sum - nums[ind] >= 0:
+                    dp[half_sum] = dp[half_sum] or prev[half_sum - nums[ind]]
+            
+       
+        
+        return dp[half]
+                
+    
+            
