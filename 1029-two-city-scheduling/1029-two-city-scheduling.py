d1b44190025f768a1c891c7a988f822c200d11c6
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-18 16:13:08 +0300

    Time: 77 ms (6.23%), Space: 18 MB (5.52%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        n = len(costs) // 2
+        
+        @cache
+        def dp(ind, countA, countB):
+            if countA == countB == n:
+                return 0
+            
+            if countA > n or countB > n:
+                return inf
+            A, B = costs[ind]
+            return min(dp(ind + 1, countA + 1, countB) + A,  dp(ind + 1, countA, countB + 1) + B)
+                
+            
+        return dp(0, 0, 0)
+            
+            
+       
+        
+        
+                
+                
+            
+        
+        
