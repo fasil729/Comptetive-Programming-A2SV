5144d6cc238c5e94d9400ca000b83e1c49f02651
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-21 15:16:32 +0300

    Time: 585 ms (60.34%), Space: 26.9 MB (21.53%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -6,15 +6,13 @@ class Solution:
             return int(x[::-1])
         diff = defaultdict(int)
         mod = 10 ** 9 + 7
-        
-        for num in nums:
-            diff[num - rev(num)] += 1
-            
         ans = 0
-        for num in diff:
-            val = diff[num]
-            ans += val * (val - 1) // 2
+        for num in nums:
+            d = num - rev(num)
+            ans += diff[d]
+            diff[d] += 1
             
+       
         return ans % mod
             
         
