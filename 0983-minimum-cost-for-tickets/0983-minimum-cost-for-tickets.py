f852d4725fa4a2019caa826c7645ffd6807c3dda
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-25 13:41:07 +0300

    Time: 88 ms (5.02%), Space: 19 MB (7.56%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,7 +1,7 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         n = len(days)
-        
+        maxi = max(days)
         
         # top down dp approach
         @cache
@@ -10,7 +10,7 @@ class Solution:
                 return 0
             day = days[ind]
             ans = inf
-            if max_day >= 365:
+            if max_day >= maxi:
                 return 0
             if max_day >= day:
                 ans = min(ans, dp(ind + 1, max_day))
@@ -18,13 +18,40 @@ class Solution:
         
         return dp(0, 0)
     
-#     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-#         n = len(days)
+    def mincostTickets2(self, days: List[int], costs: List[int]) -> int:
+        n = len(days)
         
         
-#         # bottom up dp approach
-#         maxi = max
-#         dp = []
+        # bottom up dp approach
+        maxi = max(days)
+        prev = [0] * (maxi + 1)
+       
+        n = len(days)
+        
+        for ind in range(n - 1, -1, -1):
+            dp = [inf] * (maxi + 1)
+            day = days[ind]
+            
+            for max_day in range(maxi, -1, -1):
+                index = max_day
+                if max_day >= day:
+                    dp[index] = min(dp[index], prev[index])
+                
+                new_day = index   
+                one =  prev[index] + costs[0]
+                
+                new_day = min(index + 6, maxi)
+                seven =  prev[new_day] + costs[1]
+                
+                new_day = min(index + 29, maxi)
+                thirty =  prev[new_day] + costs[2]
+                
+                dp[index] = min(dp[index], one, seven, thirty)
+            prev = dp
+        # print(dp)
+        return dp[0]
+            
+        
         
         
         
