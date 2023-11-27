f2178c3c550f6b1560c53c9f7b8f4b5f5622402f
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-27 14:25:54 +0300

    Time: 760 ms (75.03%), Space: 16.4 MB (81.60%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -20,5 +20,29 @@ class Solution:
             ans += dp(key, n - 1) 
             
         return ans % MOD
+    
+    def knightDialer(self, n: int) -> int:
+        neighbours = [[4, 6], [6, 8], [7, 9], [4, 8], [0, 3, 9], 
+                      [], [0, 1, 7], [2, 6], [1, 3], [2, 4] 
+                     ]
+        MOD = 10 ** 9 + 7
+        prev = [1] * 10
+        
+        
+        for i in range(n - 1):     
+            dp = [0] * 10
+            
+            for key in range(10):
+                
+                for neigh in neighbours[key]:
+                    dp[key] += prev[neigh] 
+                    dp[key] %= MOD
+            
+            
+            prev = dp
+        return sum(prev) % MOD
+                
+    
+    
         
         
