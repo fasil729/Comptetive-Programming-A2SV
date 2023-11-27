f748fc7a61bc9da471aed98618e2c29754d6c57d
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-11-27 14:26:14 +0300

    Time: 805 ms (70.65%), Space: 16.3 MB (81.60%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,5 +1,5 @@
 class Solution:
-    def knightDialer(self, n: int) -> int:
+    def knightDialer1(self, n: int) -> int:
         neighbours = [[4, 6], [6, 8], [7, 9], [4, 8], [0, 3, 9], 
                       [], [0, 1, 7], [2, 6], [1, 3], [2, 4] 
                      ]
