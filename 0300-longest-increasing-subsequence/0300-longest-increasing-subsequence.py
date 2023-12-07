9df927f166fbbecda3b95741b79b17c3d096d79a
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-12-08 00:42:58 +0300

    Time: 69 ms (93.34%), Space: 16.8 MB (18.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -17,7 +17,15 @@ class Solution:
         for i in range(n):
             res = max(res, dp(i))
         return res
-    # def lengthOfLIS1(self, nums: List[int]) -> int:
-    #     #binary search
+    
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)  # Find the index of the first element >= x
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
     
         
