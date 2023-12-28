f5afeb96824fb4a7ee7e096b60395983f37fe844
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-12-29 01:04:51 +0300

    Time: 2855 ms (5.00%), Space: 23.6 MB (91.00%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        
+        n = len(s)
+        def getLength(freq):
+            if freq == 1:
+                return 1
+            return len(str(freq)) + 1
+        
+        @cache
+        def dp(ind, k):
+            if k < 0:
+                return n
+            if ind == n:
+                return 0
+            
+            ans = dp(ind + 1, k - 1)
+            freq = 0
+            for i in range(ind, n):
+                if s[i] == s[ind]:
+                    freq += 1
+            
+                else:
+                    k -= 1
+                
+                ans = min(ans, dp(i + 1, k) + getLength(freq))
+            
+            return ans
+        
+        return dp(0, k)
+            
+            
+        
+        
+        
+            
+        
