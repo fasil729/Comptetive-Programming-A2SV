ba00b8d59368ec3957fbda350b76c69d601a0a18
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-12-30 10:12:53 +0300

    Time: 671 ms (43.97%), Space: 18.9 MB (5.46%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -20,3 +20,44 @@ class Solution:
         
         ans = dp(0, d)
         return ans if ans != inf else -1 
+    
+    def minDifficulty2(self, jobDifficulty: List[int], d: int) -> int:
+        
+        # bottom up dp approach
+        n = len(jobDifficulty)
+        days = d
+        prev = [0] + [inf] * days
+        
+        for ind in range(n - 1, -1, -1):
+            dp = [inf] * (days + 1) 
+            
+            for d in range(days + 1):
+                
+                maxi = 0
+                for j in range(ind, n):
+                    maxi = max(maxi, jobDifficulty[j])
+                    dp[d] = min(dp[d], prev[d - 1] + maxi)
+            print(dp)
+            prev = dp
+        print(dp) 
+        return dp[days] if dp[days] != inf else -1
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
