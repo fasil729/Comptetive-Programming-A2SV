b1d3faddbb477633dfba79d8bef96cb0d2823dd6
fasil729 <101599549+fasil729@users.noreply.github.com>
2024-01-15 23:14:15 +0300

    Time: 41 ms (51.55%), Space: 17.4 MB (32.82%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,32 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        visited = set()
-        ans = []
-        n = len(nums)
+        """
+        """
         
-        def backtrack(mask, sub):
-            if mask in visited:
-                return
-            ans.append(sub.copy())
-            visited.add(mask)
+        # intialization
+        ans = []
+        subset = []
+        n = len(nums)
+       
+        
+        def recurse(ind):
+            ans.append(subset.copy())
+            # base case
+            if ind == n:
+                return None
             
-            for i in range(n):
-                if mask & 1 << i:
-                    sub.append(nums[i])
-                    backtrack(mask ^ 1 << i, sub)
-                    sub.pop()
-            return
-        backtrack((2 ** n) - 1, [])
+            # recursive relation
+            for i in range(ind, n):
+                subset.append(nums[i])
+                recurse(i + 1)
+                subset.pop()
+            
+            
+            return None
+        
+        recurse(0)
+        
         return ans
         
+        
+       
