0a88b288ff302f95936eb4144612cd4cb8b8480b
fasil729 <101599549+fasil729@users.noreply.github.com>
2024-01-15 23:48:25 +0300

    Time: 45 ms (70.88%), Space: 17.3 MB (29.32%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,34 +1,32 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        """
+        """
+        # intalization
+        ans = 0
+        Net = []
         n = len(costs) // 2
         
-        @cache
-        def dp(ind, countA, countB):
-            if countA == countB == n:
-                return 0
-            
-            if countA > n or countB > n:
-                return inf
-            A, B = costs[ind]
-            return min(dp(ind + 1, countA + 1, countB) + A,  dp(ind + 1, countA, countB + 1) + B)
-                
-            
-        return dp(0, 0, 0)
-    
-    
-    def twoCitySchedCost2(self, costs: List[List[int]]) -> int:
-        # greedy approach
-        n = len(costs)
         
-        costs.sort(key=lambda x: x[1] - x[0])
-        ans = 0
+        # construct net array
         for ind, cost in enumerate(costs):
-            costA, costB = cost
-            if ind < n // 2:
-                ans += costB
+            costa, costb = cost
+            Net.append((costa - costb, ind))
+        
+        # sort in ascending order
+        Net.sort()
+        
+        # compute cost
+        for i, net in enumerate(Net):
+            ind = net[1]
+            if i < n:
+                ans += costs[ind][0]
             else:
-                ans += costA
+                ans += costs[ind][1]
+        
         return ans
+        
+        
             
             
        
